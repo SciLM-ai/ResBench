ed2ddc5561383582db2315685f4a6d7ce4c52755
Ilgar Baghishov <baghishov@utexas.edu>
2026-07-31 06:43:43 -0500

    Comparison builder: add wave-3 environments (CB_LABYRINTH, CB_JIGSAW)

--- analysis/build_specialists_comparison.py
+++ analysis/build_specialists_comparison.py
@@ -41,6 +41,8 @@ ENVS = [
     ('lobe', 'lobe', 'specialist (matched 40 ep)'),
     ('delta', 'delta_80ep', 'specialist (80 ep, direct)'),
     ('channel:SH_DISTAL', 'sh_distal_80ep', 'specialist (80 ep, direct)'),
+    ('channel:CB_LABYRINTH', 'cb_labyrinth_80ep', 'specialist (80 ep, direct)'),
+    ('channel:CB_JIGSAW', 'cb_jigsaw_80ep', 'specialist (80 ep, direct)'),
 ]
 
 CELL_LABELS = {
